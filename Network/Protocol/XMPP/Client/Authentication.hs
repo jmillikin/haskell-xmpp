bd216cc4771f20ab3cb8af2de8f2219b2a82aca2
John Millikin <jmillikin@gmail.com>
2010-04-15 02:14:11 +0000

    Moved client-related modules into a subpackage.

--- Network/Protocol/XMPP/Client/Authentication.hs
+++ Network/Protocol/XMPP/Client/Authentication.hs
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-module Network.Protocol.XMPP.Authentication
+module Network.Protocol.XMPP.Client.Authentication
 	( Result(..)
 	, authenticate
 	) where

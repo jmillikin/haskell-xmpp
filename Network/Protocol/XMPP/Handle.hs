21fada263c689cfc39bba931748afc682a2fea9d
John Millikin <jmillikin@gmail.com>
2010-04-26 17:01:15 +0000

    Small tweak for first public version of GNU TLS bindings.

--- Network/Protocol/XMPP/Handle.hs
+++ Network/Protocol/XMPP/Handle.hs
@@ -46,10 +46,9 @@ liftTLS' io = do
 
 startTLS :: Handle -> ErrorT T.Text IO Handle
 startTLS (SecureHandle _ _) = E.throwError "Can't start TLS on a secure handle"
-startTLS (PlainHandle h) = liftTLS' $ TLS.runClient $ do
-	TLS.setTransport $ TLS.transportHandle h
+startTLS (PlainHandle h) = liftTLS' $ TLS.runClient (TLS.handleTransport h) $ do
 	TLS.setPriority [TLS.X509]
-	TLS.setCredentials TLS.certificateCredentials
+	TLS.setCredentials =<< TLS.certificateCredentials
 	TLS.handshake
 	SecureHandle h `fmap` TLS.getSession
 
